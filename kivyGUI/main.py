89bb4690feb7a7fb185a7b4559b2348545b20161
ManuelPartidaG <35499030+ManuelPartidaG@users.noreply.github.com>
2020-07-19 16:50:58 -0700

    Added Proxy screen

--- kivyGUI/main.py
+++ kivyGUI/main.py
@@ -20,6 +20,15 @@ from selenium.webdriver.chrome.options import Options
 import zipfile,os
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.image import Image
+import zipfile
+
+
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+import os, sys
+import time
+from bs4 import BeautifulSoup
 
 class ImageButton(ButtonBehavior, Image):
     pass
@@ -34,6 +43,10 @@ class ContentNavigationDrawer(BoxLayout):
     input_City=ObjectProperty(None)
     input_State=ObjectProperty(None)
     input_Zip=ObjectProperty(None)
+    pHost=ObjectProperty(None)
+    pPort=ObjectProperty(None)
+    pUser=ObjectProperty(None)
+    pPass=ObjectProperty(None)
     def save_info(self):
         Fname = self.input_Fname.text
         Lname = self.input_Lname.text
@@ -91,6 +104,178 @@ class ContentNavigationDrawer(BoxLayout):
             driver.find_element_by_xpath(xPath_Submit).click()
             time.sleep(30)
             print("All Done with " +str(email)+ "!")
+    def get_chromedriver(use_proxy=False, user_agent=None):
+        path = os.path.dirname(os.path.abspath(__file__))
+        chrome_options = webdriver.ChromeOptions()
+        if use_proxy:
+            pluginfile = 'proxy_auth_plugin.zip'
+
+            with zipfile.ZipFile(pluginfile, 'w') as zp:
+                zp.writestr("manifest.json", manifest_json)
+                zp.writestr("background.js", background_js)
+            chrome_options.add_extension(pluginfile)
+        if user_agent:
+            chrome_options.add_argument('--user-agent=%s' % user_agent)
+        driver = webdriver.Chrome(
+            os.path.join("/Users/manuelpartida/Desktop/chromedriver"),
+            chrome_options=chrome_options)
+        return driver
+
+    def main():
+        driver = get_chromedriver(use_proxy=True)
+        driver.get('https://www.google.com/search?q=my+ip+address')
+        time.sleep(30)
+
+
+    def audioToText(mp3Path):
+        driver = get_chromedriver(use_proxy=True)
+        driver.execute_script('''window.open("","_blank");''')
+        driver.switch_to.window(driver.window_handles[1])
+
+        driver.get(googleIBMLink)
+
+        # Upload file
+        time.sleep(1)
+        root = driver.find_element_by_id('root').find_elements_by_class_name('dropzone _container _container_large')
+        btn = driver.find_element(By.XPATH, '//*[@id="root"]/div/input')
+        btn.send_keys(mp3Path)
+
+        # Audio to text is processing
+        time.sleep(audioToTextDelay)
+
+        text = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[6]/div/div/dialog').find_elements_by_tag_name('dd')
+        result = " ".join([each.text for each in text])
+
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+
+        return result
+
+
+    def saveFile(content, filename):
+        with open(filename, "wb") as handle:
+            for data in content.iter_content():
+                handle.write(data)
+    def testproxy(self):
+        delayTime = 2
+        audioToTextDelay = 10
+        filename = 'test.mp3'
+        byPassUrl = 'https://www.google.com/recaptcha/api2/demo'
+        googleIBMLink = 'https://speech-to-text-demo.ng.bluemix.net/'
+
+        option = webdriver.ChromeOptions()
+        option.add_argument('--disable-notifications')
+        option.add_argument("--mute-audio")
+        # option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+        option.add_argument(
+            "user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1")
+
+        manifest_json = """
+        {
+            "version": "1.0.0",
+            "manifest_version": 2,
+            "name": "Chrome Proxy",
+            "permissions": [
+                "proxy",
+                "tabs",
+                "unlimitedStorage",
+                "storage",
+                "<all_urls>",
+                "webRequest",
+                "webRequestBlocking"
+            ],
+            "background": {
+                "scripts": ["background.js"]
+            },
+            "minimum_chrome_version":"22.0.0"
+        }
+        """
+
+        background_js = """
+        var config = {
+                mode: "fixed_servers",
+                rules: {
+                singleProxy: {
+                    scheme: "http",
+                    host: "%s",
+                    port: parseInt(%s)
+                },
+                bypassList: ["localhost"]
+                }
+            };
+        chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+        function callbackFn(details) {
+            return {
+                authCredentials: {
+                    username: "%s",
+                    password: "%s"
+                }
+            };
+        }
+        chrome.webRequest.onAuthRequired.addListener(
+                    callbackFn,
+                    {urls: ["<all_urls>"]},
+                    ['blocking']
+        );
+        """ % (self.pHost.text, self.pPort.text, self.pUser.text, self.pPass.text)
+        driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=option)
+        driver.get(byPassUrl)
+
+        googleClass = driver.find_elements_by_class_name('g-recaptcha')[0]
+        outeriframe = googleClass.find_element_by_tag_name('iframe')
+        outeriframe.click()
+
+        allIframesLen = driver.find_elements_by_tag_name('iframe')
+        audioBtnFound = False
+        audioBtnIndex = -1
+
+        for index in range(len(allIframesLen)):
+            driver.switch_to_default_content()
+            iframe = driver.find_elements_by_tag_name('iframe')[index]
+            driver.switch_to.frame(iframe)
+            driver.implicitly_wait(delayTime)
+            try:
+                audioBtn = driver.find_element_by_id('recaptcha-audio-button') or driver.find_element_by_id('recaptcha-anchor')
+                audioBtn.click()
+                audioBtnFound = True
+                audioBtnIndex = index
+                break
+            except Exception as e:
+                pass
+
+        if audioBtnFound:
+            try:
+                while True:
+                    href = driver.find_element_by_id('audio-source').get_attribute('src')
+                    response = requests.get(href, stream=True)
+                    saveFile(response, filename)
+                    response = audioToText(os.getcwd() + '/' + filename)
+                    print(response)
+
+                    driver.switch_to_default_content()
+                    iframe = driver.find_elements_by_tag_name('iframe')[audioBtnIndex]
+                    driver.switch_to.frame(iframe)
+
+                    inputbtn = driver.find_element_by_id('audio-response')
+                    inputbtn.send_keys(response)
+                    inputbtn.send_keys(Keys.ENTER)
+
+                    time.sleep(2)
+                    errorMsg = driver.find_elements_by_class_name('rc-audiochallenge-error-message')[0]
+                    audioToText(response)# maybe goes here
+                    if errorMsg.text == "" or errorMsg.value_of_css_property('display') == 'none':
+                        print("Success")
+                        break
+
+            except Exception as e:
+                print(e)
+                print('Caught. Need to change proxy now')
+        else:
+            print('Button not found. This should not happen.')
+        #
+        # if __name__ == '__main__':
+        #     audioToText()
+
 
         #self.submit(userList)
     pass
